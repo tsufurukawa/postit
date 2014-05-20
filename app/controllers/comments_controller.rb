8ebb80dbc315e6d930867d7607cf52d21aceef79
Tsubasa Furukawa <tfurukawa824@gmail.com>
2014-05-19 17:21:28 -0700

    Finished Authenticatin and Added User Edit/Show Profile

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,10 +1,12 @@
 class CommentsController < ApplicationController
+  before_action :require_user
+
   def create
     @post = Post.find(params[:post_id])
     @comment = Comment.new(params.require(:comment).permit(:body))
     @comment.post = @post
 
-    @comment.creator = User.first # TODO: Fix after authentication
+    @comment.creator = current_user # TODO: Fix after authentication
 
     if @comment.save
       flash[:notice] = "You created a new comment"
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,50 @@
+class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update]
+  before_action :require_same_user, only: [:edit, :update]
+
+  def new
+    @user = User.new
+  end
+
+  def show; end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      flash[:notice] = "You successfully registered and logged in."
+      session[:user_id] = @user.id
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def edit; end
+
+  def update
+    if @user.update(user_params)
+      flash[:notice] = "You updated your profile."
+      redirect_to user_path(@user)
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :password)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def require_same_user
+    if current_user != @user
+      flash[:error] = "You're not allowed to do that."
+      redirect_to root_path
+    end
+  end
+end
